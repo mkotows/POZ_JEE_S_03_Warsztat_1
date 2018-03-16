a91db1c12d815fde73173de0285aceec842471d4
Mateusz Kotowski <kotas55@wp.pl>
2018-03-16 13:17:51 +0100

    zadanie 1 zrobione

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,9 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Zadanie1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random rand = new Random();
+		int guessedNumber = rand.nextInt(100)+1;
+		System.out.println("----- " + guessedNumber);
+		System.out.println("Zgadnij liczbę: ");
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			
+			while(!scan.hasNextInt()) {
+				System.out.println("Podaj liczbę!!");
+				scan.next();
+			}
+			int num = scan.nextInt(); 
+			
+			if(num==guessedNumber) {
+				System.out.println("Zgadłeś");
+				break;
+			} else if(num>guessedNumber){
+				System.out.println("Za dużo!");
+			} else {
+				System.out.println("Za mało!");
+			}
+			
+		}
+		
 	}
 
 }
